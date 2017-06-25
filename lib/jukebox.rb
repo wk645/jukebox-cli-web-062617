46fac58f6ae665138e47968f8afa6937cc5f0ee0
wk645 <wk645@nyu.edu>
2017-06-25 16:14:30 -0400

    jukebox cli

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -18,23 +18,25 @@ def help
     - exit: exits this program"
 end
 
-def list(songs)
-    songs.each_with_index do |song, index|
-    puts "#{index + 1}. #{song}"
-    end
-end
-
 def play(songs)
     puts "Please enter a song name or number:"
     response = gets.chomp
     if (1..songs.length).include?(response.to_i)
         puts "Playing #{songs[response.to_i - 1]}"
-    elsif songs.include?(response)
+        elsif songs.include?(response)
         puts "Playing #{response}"
-    else
+        else
         puts "Invalid input, please try again"
     end
 end
+
+def list(songs)
+    songs.each_with_index do |index, song|
+    puts "#{index}. #{song}"
+    end
+end
+
+
 
 def exit_jukebox
     puts "Goodbye"
@@ -42,22 +44,23 @@ end
 
 def run(songs)
     help
-    puts "Please enter a command:"
-    response = gets.chomp
-    
-    until response == "exit"
-        if response == "list"
+    while true
+        puts "Please enter a command:"
+        response = gets.chomp
+        case response
+        when "list"
             list(songs)
-            response == gets.chomp
-        elsif response == "play"
+        when "play"
             play(songs)
-            response == gets.chomp
-        elsif response == "help"
+        when "help"
             help
-            response == gets.chomp
+        when "exit"
+            exit_jukebox
+        break
+        else
+        help
         end
     end
-    exit_jukebox
 end
         
 
